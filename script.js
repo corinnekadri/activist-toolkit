1254e7f9e140209c7cbad87c0771e1756a05e5ec
Glitch (hello-webpage) <none>
2020-07-15 21:49:12 +0000

    👙🏥 Checkpoint ./abortion.css:18533656/8454 ./menstrual-products.css:18533656/10249 ./resources.css:18533656/4317 ./script.js:18533656/1310 ./take-action.css:18533656/10406 ./take-action.html:18533656/9389

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
 /************************************************/
 /********* ADD CHECKLIST FUNCTIONALITY **********/
 /************************************************/
-var fullItemList= document.getElementById("my-list");
+var fullItemList = document.getElementById("my-list");
 
-if (fullItemList){
-fullItemList.addEventListener("click", checkOffItem);
+if (fullItemList) {
+  fullItemList.addEventListener("click", checkOffItem);
 }
 
-function checkOffItem(clicked){
-  if (clicked.target.tagName=="LI"){
+function checkOffItem(clicked) {
+  if (clicked.target.tagName == "LI") {
     clicked.target.classList.toggle("all-done");
   }
 }
 /* TODO: Add a variable to store the "my-list" element */
 
-
 /* TODO: Create the event listener that listens for a mouse click and runs the checkOffList function */
 
-
 /* TODO: Declare the function checkOffList and add actions inside the { } */
-
-
